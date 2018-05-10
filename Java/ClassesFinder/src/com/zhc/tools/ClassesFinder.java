a122366dd4339623bbf8833cbe2129075b50d363
Zhang <hczhang@amazon.com>
2018-05-10 18:24:49 +0800

    add class name for args capability

--- Java/ClassesFinder/src/com/zhc/tools/ClassesFinder.java
+++ Java/ClassesFinder/src/com/zhc/tools/ClassesFinder.java
@@ -4,10 +4,19 @@ import java.util.Set;
 
 import org.reflections.Reflections;
 
+/**
+ * Find all classes or interfaces that extends or implements a super type.
+ * Author: Hongchang
+ */
 public class ClassesFinder {
+    /**
+     * args[0] is the class or interface name such as com.zhc.services.VerificationService
+     * @param args
+     * @throws Exception
+     */
     public static void main(String[] args) throws Exception {
         Reflections reflections = new Reflections(".*");
-        Class clazz = Class.forName("com.amazon.identity.cvf.verification.VerificationService");
+        Class clazz = Class.forName(args[0]);
 
         Set<Class> subClasses = reflections.getSubTypesOf(clazz);
         for (Class c : subClasses) {
