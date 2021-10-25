215505e7f1dc2f9b68ac5d0ea2436f022e564188
Hongchang Zhang <hczhang@amazon.com>
2021-10-24 21:22:00 -0700

    stock selector cache

---
+++ stock/stock_sel_basic_cache.py
@@ -0,0 +1,60 @@
+import akshare as ak
+#import csv
+import pandas as pd
+from datetime import date 
+from os import path 
+
+# save and check stock metadata cache.
+force_refresh = False
+stock_metadata_cache_filepath = '/Users/hczhang/amazon_laptops/Personal/stock/stock_metadata_cache.csv'
+if path.isfile(stock_metadata_cache_filepath) and not force_refresh:
+    stock_metadata_cache_df = pd.read_csv(stock_metadata_cache_filepath)
+else:
+    # fetch all existing stock's current snapshot and save cache
+    stock_zh_a_spot_df = ak.stock_zh_a_spot()
+    stock_zh_a_spot_df.to_csv(stock_metadata_cache_filepath, header=True) 
+
+
+# check cache file 
+stock_cache_filepath = '/Users/hczhang/amazon_laptops/Personal/stock/stock_cache.csv'
+stock_cache_df = pd.read_csv(stock_cache_filepath)
+
+
+for index, row in stock_zh_a_spot_df.iterrows():
+    # sh600000, sz......
+    stock_code = row['代码'] 
+    print('stock_code: ' + stock_code)
+    #if stock_code in stock_cache_df.stock_code:
+    if stock_cache_df['stock_code'].str.contains(stock_code).any():
+        print('Existed, skip: ' + stock_code)
+        continue
+    
+    
+    short_stock_code = stock_code[2:]
+    print('short_stock_code: ' + short_stock_code)
+    stock_zh_a_hist_df = ak.stock_zh_a_hist(symbol=short_stock_code, period="daily", start_date="20170301", end_date='21211107', adjust="qfq")
+    cur_price = stock_zh_a_hist_df[['收盘']].iat[-1,0]
+    print('cur_price: ' + str(cur_price) + '(' + str(type(cur_price)) + ')')
+    
+    stock_financial_analysis_indicator_df = ak.stock_financial_analysis_indicator(stock=short_stock_code)
+    cur_unassigned_benefit = stock_financial_analysis_indicator_df[['每股未分配利润(元)']].iat[0,0]
+    cur_unassigned_benefit = float(cur_unassigned_benefit)
+    print('cur_unassigned_benefit: ' + str(cur_unassigned_benefit) +'(' + str(type(cur_unassigned_benefit)) + ')')
+    
+    # Cache handler
+    cur_date = date.today().strftime("%Y/%m/%d")
+    cur_item_data = [{'stock_code': stock_code, 'cur_price': cur_price, 'cur_unassigned_benefit':cur_unassigned_benefit, 'date':cur_date}]
+    cur_item_df = pd.DataFrame(cur_item_data)
+    cur_item_df.to_csv(stock_cache_filepath, mode='a', header=False) 
+    
+    
+    result_dic = {}
+    if cur_unassigned_benefit >= cur_price:
+        result_dic[stock_code] = cur_unassigned_benefit
+        #with open('stock_sel_result.dat', 'a') as file:
+            #file.write(stock_code + " " + str(cur_price) + " " + str(cur_unassigned_benefit) + "\n")
+    
+    result_dic
+
+
+
